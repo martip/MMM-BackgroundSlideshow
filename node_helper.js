349ed7fd32f80d04b00cb9ae9b26dabd6cb7d966
Paolo Martino <martip@martip-macbook.local>
2024-10-10 14:02:37 +0200

    Added limit

--- node_helper.js
+++ node_helper.js
@@ -128,6 +128,9 @@ module.exports = NodeHelper.create({
           config.sortImagesBy,
           config.sortImagesDescending
         );
+    // Limit to 10 photos
+    this.imageList = this.imageList.slice(0, 10);
+
     Log.info(`BACKGROUNDSLIDESHOW: ${this.imageList.length} files found`);
     this.index = 0;
 
