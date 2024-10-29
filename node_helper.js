7659c152df3258bdc1a11c690730124f7204a393
martip <martip@gmail.com>
2024-10-29 09:14:35 +0100

    Fixed log

--- node_helper.js
+++ node_helper.js
@@ -367,7 +367,7 @@ module.exports = NodeHelper.create({
     } else if (notification === 'BACKGROUNDSLIDESHOW_CONTROL') {
       Log.log(`Socket Notification Received: BACKGROUNDSLIDESHOW_CONTROL #367`);
       Log.log(`Payload: ${JSON.stringify(payload, null, 2)}`);
-      Log.log(`Timer: ${JSON.stringify(timer, null, 2)}`);
+      Log.log(`Timer: ${JSON.stringify(this.timer, null, 2)}`);
       if (payload) {
         if (payload === 'on' && !this.timer) {
           this.sendSocketNotification('BACKGROUNDSLIDESHOW_PLAY');
